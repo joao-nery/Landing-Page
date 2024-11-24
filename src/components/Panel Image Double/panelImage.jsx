b2a2613a428b273723cf90bce6ae4d200a981e0b
João Nery <joaonery87@gmail.com>
2024-11-24 16:40:52 -0300

    new file:   src/Images/img.png 	modified:   src/components/Panel Image Double/panelImage.jsx

--- src/components/Panel Image Double/panelImage.jsx
+++ src/components/Panel Image Double/panelImage.jsx
@@ -1,10 +1,11 @@
 import imageExemple from "../../Images/ImageExemple.png";
+import image from './../../Images/img.png';
 
 export function PanelImage() {
   return (
     <div className="flex gap-10 justify-center p-10">
       <img className="h-28 lg:h-72 sm:h-52" src={imageExemple} alt="Image" />
-      <img className="h-28 lg:h-72 sm:h-52" src={imageExemple} alt="Image" />
+      <img className="h-28 lg:h-72 sm:h-52" src={image} alt="Image" />
     </div>
   );
 }
