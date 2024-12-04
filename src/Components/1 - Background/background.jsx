dcd337eee42724e7ca135256d35ac906dd4db113
João Nery <joaonery87@gmail.com>
2024-12-04 06:55:33 -0300

    updates

--- src/Components/1 - Background/background.jsx
+++ src/Components/1 - Background/background.jsx
@@ -1,7 +1,6 @@
 import { Hero } from "../3 - Hero/hero"
 import { SocialProof } from "../4 - SocialProof/socialproof"
 import { Benefits } from "../5 - Benefits/benefits"
-import { Newsletter } from "../6 - Newsletter/newsletter"
 import { Footer } from "../7 - footer/footer.jsx"
 
 import './background.modules.css';
@@ -13,7 +12,6 @@ export function Background(){
                 <Hero/>
                 <SocialProof/>
                 <Benefits/>
-                <Newsletter/>
                 <Footer/>
             </div>
         </main>
--- src/Components/3 - Hero/hero.jsx
+++ src/Components/3 - Hero/hero.jsx
@@ -2,7 +2,7 @@ import './hero.modules.css';
 export function Hero(){
     return (
         <div className='title-hero'>
-            <h1>Nós desenvolvemos sites <b>incríveis</b> para o seu negocio</h1>
+            <h1>Nós desenvolvemos sites <b>incríveis</b> para o seu negocio!</h1>
             <p>Lorem Ipsumis simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.</p>
 
             <div className='buttons'>
